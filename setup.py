3de96aa29589a777bff31fbd3936498ed876a978
Ian Foote <ian@incuna.com>
2015-02-20 12:21:30 +0000

    Add BSD license

--- setup.py
+++ setup.py
@@ -18,10 +18,12 @@ setup(
     url='https://github.com/incuna/incuna-pigeon',
     author='Incuna',
     author_email='admin@incuna.com',
+    license='BSD',
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
         'Topic :: Communications',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
     ],
